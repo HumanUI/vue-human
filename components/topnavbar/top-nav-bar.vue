92a3c54acee626925cb264641db5be3821315bfc
weiwei <weiwei@suiyi.com.cn>
2016-11-11 15:37:46 +0800

    add topnavbar

---
+++ components/topnavbar/top-nav-bar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div :class="cssPrefix">
+    <div :class="[cssPrefix + '-left']">
+      <slot name="left"></slot>
+    </div>
+    <h1 v-if="$slots.title">
+      <mn-icon name="load-a" spin v-if="loading"></mn-icon>
+      <slot name="title"></slot>
+    </h1>
+    <div :class="[cssPrefix+'-center']" v-if="$slots.center">
+      <slot name="center"></slot>
+    </div>
+    <div :class="[cssPrefix +'-right']">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+      }
+    },
+    props: {
+      loading: {
+        type: Boolean,
+        default: false
+      }
+    },
+    computed: {
+      cssPrefix () {
+        return `${this.$human.cssPrefix}top-nav-bar`
+      }
+    }
+  }
+</script>
---
+++ examples/docs/components/topnavbar/top-nav-bar.vue
@@ -0,0 +1,69 @@
+<template>
+  <div> 
+    <mn-card>
+      <mn-top-nav-bar>
+        <router-link to="/" slot="left">
+          <mn-btn >
+            <mn-icon name="ios-arrow-left"></mn-icon>back
+          </mn-btn>
+        </router-link>
+        <template slot="title">text and icon ,hide lang text</template>
+        <mn-btn slot="right" @click.native.prevent="getEle()">
+          share
+        </mn-btn>
+      </mn-top-nav-bar>
+      <mn-top-nav-bar>
+        <router-link to="/" slot="left">
+          <mn-btn >
+            <mn-icon name="ios-arrow-left"></mn-icon>
+          </mn-btn>
+        </router-link>
+        <template slot="title">just icon</template>
+        <mn-btn slot="right" @click.native.prevent="doSomething()">
+          <mn-icon name="more"></mn-icon>
+        </mn-btn>
+      </mn-top-nav-bar>
+      <mn-top-nav-bar loading>
+        <router-link to="/" slot="left">
+          <mn-btn >
+            <mn-icon name="ios-arrow-left"></mn-icon>
+          </mn-btn>
+        </router-link>
+        <template slot="title">loading</template>
+      </mn-top-nav-bar>
+      <mn-top-nav-bar>
+        <router-link to="/" slot="left">
+          <mn-btn >
+            <mn-icon name="ios-arrow-left"></mn-icon>
+          </mn-btn>
+        </router-link>
+        <mn-search-bar slot="center"></mn-search-bar>
+      </mn-top-nav-bar>
+    </mn-card>
+  </div>
+</template>
+  
+<script>
+  export default {
+    data () {
+      return {
+      }
+    },
+    methods: {
+      doSomething () {
+        window.alert('hello')
+      },
+      getEle () {
+        console.log(this.$el)
+      }
+    }
+  }
+</script>
+<style scoped lang="scss">
+  .card{
+    padding-top: 15px;
+    & > *{
+      margin-bottom: 15px;
+    }
+  }
+</style>
