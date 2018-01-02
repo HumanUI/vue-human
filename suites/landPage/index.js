a675dabe84419fa83ff1da97963358533ae7841a
zhouxiao <zhouxiao@zhouxiao.local>
2018-01-02 11:33:02 +0800

    to vue-human

---
+++ suites/landPage/index.js
@@ -0,0 +1,8 @@
+import Suites from '../../utils/Suites'
+import landPage from './landPage'
+import landPageItem from './landPage-item'
+
+export default new Suites([
+  landPage,
+  landPageItem
+])
