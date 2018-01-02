93fdde74dbb11b64620c88fd378ab0082d3cb907
zhouxiao <zhouxiao@zhouxiao.local>
2018-01-02 14:15:49 +0800

    example

--- examples/src/components/landPage/index.vue
+++ examples/src/components/landPage/index.vue
@@ -1,10 +1,19 @@
 <template>
   <page>
-    <mn-landPage :turnPageBtn='true'>
-      <mn-landPage-item :itemIndex='0'>This is 1 item.</mn-landPage-item>
+    <mn-landPage style="background-color: #dbfffa" turnPageBtn pageNumber >
+      <!-- 
+      <mn-landPage-item :itemIndex='0'>
+        <div class="mn-landPage__item-wrapper"></div>
+      </mn-landPage-item>
       <mn-landPage-item :itemIndex='1'>This is 2 item.</mn-landPage-item>
       <mn-landPage-item :itemIndex='2'>This is 3 item.</mn-landPage-item>
-      <mn-landPage-item :itemIndex='3'>This is 4 item.</mn-landPage-item>
+      <mn-landPage-item :itemIndex='3'>This is 4 item.</mn-landPage-item> 
+      -->
+      <mn-landPage-item v-for="index in 5" :key="index" :itemIndex='index - 1'>
+        <div class="mn-landPage__item-wrapper">
+          This is {{index}} item.
+        </div>
+      </mn-landPage-item>
     </mn-landPage>
   </page>
 </template>
@@ -19,5 +28,15 @@ export default {
 </script>
 
 <style>
-
+.mn-landPage__item-wrapper {
+  padding: 20px;
+  width: 100%;
+  height: 100%;
+  border: 1px solid #ebeef5;
+  border-radius: 4px;
+  overflow: hidden;
+  color: #303133;  
+  background-color: #fff;
+  box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+}
 </style>
